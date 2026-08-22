caf8b0148f4b795608bdff6633164445f567ab72
Rana Muzamil <mmlrana00@gmail.com>
2026-08-22 12:28:21 +0500

    wrap advanced routes in a shared page shell

---
+++ components/dashboard/advanced/index.ts
@@ -0,0 +1,4 @@
+export { AdvancedToolsHub } from './AdvancedToolsHub';
+export { AdvancedPageShell } from './AdvancedPageShell';
+export { AdvancedContextBar } from './AdvancedContextBar';
+export { RelatedToolsRail } from './RelatedToolsRail';
